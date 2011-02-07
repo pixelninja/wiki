3003b27f3e72907b3e01c1d31e678fe5615399e5
Rowan Lewis <me@rowanlewis.com>
2011-02-07 12:13:19 +1000

    Removed docs directory, automatically created on execution instead.

--- wiki/app.php
+++ wiki/app.php
@@ -8,6 +8,12 @@
 		protected function initialize() {
 			$this->settings = new \Libs\Collections\Parameters();
 			$this->settings()->{'read-only'} = true;
+			
+			// Create new docs directory?
+			if (!is_dir($this->dir . '/docs')) {
+				mkdir($this->dir . '/docs');
+				copy($this->dir . '/assets/wiki.html', $this->dir . '/docs/index.html');
+			}
 		}
 		
 		public function settings() {
